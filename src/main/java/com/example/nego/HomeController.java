3bd76bdfcd2eaaec488b668723e717217b287db9
NUCBOX_M5\kkim0 <kkim01201@naver.com>
2024-08-13 10:41:37 +0900

    [수정][메인화면 리스트 띄우기 셋팅중]

--- src/main/java/com/example/nego/HomeController.java
+++ src/main/java/com/example/nego/HomeController.java
@@ -1,13 +1,15 @@
 package com.example.nego;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-import jakarta.mail.internet.InternetAddress;
-import jakarta.mail.internet.MimeMessage;
+import com.example.nego.cate.service.CateService;
+import com.example.nego.dto.CateDTO;
+
 import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
@@ -16,8 +18,24 @@ public class HomeController {
 	@Autowired
 	JavaMailSender mailSender;
 	
+	@Autowired
+	CateService cateService;
+
+	
 	@GetMapping(value = {"/", "/main.do"})
 	public String main(HttpServletRequest req) {
+		List<CateDTO> mainList = cateService.mainList();
+		req.setAttribute("mainList", mainList);
+		
+		for (CateDTO mainCategory : mainList) {
+			List<CateDTO> subList = cateService.subList(mainCategory.getId());
+			mainCategory.setSubList(subList);
+			
+			for (CateDTO subCategory : subList) {
+				List<CateDTO> itemList = cateService.itemList(subCategory.getId());
+				subCategory.setItemList(itemList);
+			}  
+		}
 		return "main";
 	}
 	
--- src/main/java/com/example/nego/cate/service/CateService.java
+++ src/main/java/com/example/nego/cate/service/CateService.java
@@ -1,8 +1,12 @@
 package com.example.nego.cate.service;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.nego.dto.CateDTO;
 
 @Service
 public class CateService {
@@ -10,6 +14,21 @@ public class CateService {
 	@Autowired
 	private SqlSession sqlSession;
 	
+
+	public List <CateDTO> mainList(){
+		
+		return sqlSession.selectList("mainList");
+	}
+	
+	public  List <CateDTO> subList(int id){
+		return sqlSession.selectList("subList",id);
+			
+	}
+	
+	public  List <CateDTO> itemList(int id){
+		return sqlSession.selectList("itemList",id);
+		
+	}
 
 	
 }
